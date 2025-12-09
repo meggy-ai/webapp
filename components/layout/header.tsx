5091e38a8913773e4644006a06b38174fedea59e
sameershaik-coder <sameershaik.coder@gmail.com>
2025-12-09 17:33:33 +0530

    fix: resolve CSS import order causing 500 error
    - Move Google Fonts import to top of CSS file
    - Fix avatar placeholder image reference
    - Resolve Next.js CSS parsing error

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -53,7 +53,7 @@ export function Header() {
               <DropdownMenuTrigger asChild>
                 <Button variant="ghost" className="relative h-8 w-8 rounded-full">
                   <Avatar className="h-8 w-8">
-                    <AvatarImage src="/placeholder-avatar.jpg" alt="@user" />
+                    <AvatarImage src="" alt="@user" />
                     <AvatarFallback>U</AvatarFallback>
                   </Avatar>
                 </Button>
